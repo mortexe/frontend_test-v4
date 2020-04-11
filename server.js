4317a6461cf909caf44c55afff0f79fdc0298e61
Paris Chrysanthou <paris.chrysanthou@gmail.com>
2020-04-11 13:42:54 +0300

    feat: server data handling

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 5000;
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.listen(port, () => console.log(`Running server on port ${port}`));
+
+app.get('/fetch_videos', (req, res) => {
+    const dir = path.join(__dirname, '/src/api/data');
+    const data = fs.readdirSync(dir)
+        .filter(file => path.extname(file) === '.json')
+        .map(name => require(path.join(dir, name)));
+
+    const joinedData = data[0].concat(data[1]);
+    res.send(joinedData);
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import path from 'path';
 import SearchButton from './components/SearchButton.jsx';
 import Filters from './components/Filters.jsx';
+import Video from './components/Video.jsx';
 
 export default function App() {
   const handleSearch = (input) => {
     console.log(input.target.value);
   };
+
+  useEffect(() => {
+    fetch('http://localhost:5000/fetch_videos')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
 
   return (
     <>
@@ -16,7 +31,9 @@ export default function App() {
         <SearchButton input={handleSearch} />
         <Filters />
       </div>
-      <div>{/* <h1>Video Section</h1> */}</div>
+      <div>
+        <h1>Video Section</h1>
+      </div>
     </>
   );
 }
