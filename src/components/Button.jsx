88fdef1694eed01a0ce5c5239ec384806952929e
Paris Chrysanthou <paris.chrysanthou@gmail.com>
2020-04-11 13:43:22 +0300

    filter button switching

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Button(props) {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    props.active === props.id ? setIsActive(true) : setIsActive(false);
+  }, [props.active]);
   return (
-    <div onClick={props.onClick} className="button-container">
+    <div
+      onClick={props.onClick}
+      className={`${isActive ? 'button-container-active' : 'button-container'}`}
+    >
       <p>{props.text}</p>
     </div>
   );
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,17 +1,28 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from './Button.jsx';
 
 export default function Filters(props) {
   const filterOptions = ['Newest first', 'Oldest first', 'Rating'];
-  const handleFilterSwitch = useCallback((button) => {
-    console.log('clicked: ' + button);
-  }, []);
+  const [active, setActive] = useState(0);
+
+  const handleFilterSwitch = useCallback(
+    (index) => {
+      setActive(index);
+    },
+    [setActive]
+  );
 
   return (
     <div className="filter-container">
       <p className="title">Order By</p>
       {filterOptions.map((item, index) => (
-        <Button key={index} onClick={handleFilterSwitch} text={item} />
+        <Button
+          key={index}
+          id={index}
+          active={active}
+          onClick={() => handleFilterSwitch(index)}
+          text={item}
+        />
       ))}
     </div>
   );
