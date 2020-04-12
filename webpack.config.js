173191badb88cd87dbfb15b5ee2369c66ebc22c3
Paris Chrysanthou <paris.chrysanthou@gmail.com>
2020-04-12 21:38:12 +0300

    webpack host + bundle

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,8 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'),
+        host: '0.0.0.0',
+        port: '8082'
     }
 };
